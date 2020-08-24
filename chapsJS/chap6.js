0a5593bb6a034d7973e215a2cf63089adc22458c
haarris <haarris82@gmail.com>
2020-08-24 15:17:43 +1000

    first question done and some notes

--- chapsJS/chap6.js
+++ chapsJS/chap6.js
@@ -165,6 +165,9 @@ temp.fahrenheit = 86; console.log(temp.fahrenheit); //86 console.log(temp.celsiu
 //this.celsius = (value-32) / 1.8; -> 86-32 = 54, 54/1.8 = 30
   
 Inheritance:
+
+  - JS allows you to create ('extends') a new (subclass) Class from copying another old Class (superclass)and adjust it
+  
   
   
   
@@ -178,3 +181,54 @@ Inheritance:
 
 
 */
+
+
+Exercises:
+
+A vector type:
+
+
+// Your code here.
+class Vec {
+  constructor (x,y) {
+    this.x = x;
+    this.y = y;
+  }
+ 
+   plus(vecInstance) {
+     /*
+     console.log(`Returned x:`+
+	 	` ${this.x + vecInstance.x} and y: ${this.y + vecInstance.y}`);
+     return "hello";
+     */
+     let vecInstance2 = new Vec(this.x+vecInstance.x, this.y+vecInstance.y);
+     return {vecInstance2};
+   }
+   minus(vecInstance) {
+     /*
+     console.log(`Returned x:`+
+	 	` ${this.x + vecInstance.x} and y: ${this.y + vecInstance.y}`);
+     return "hello";
+     */
+     let vecInstance2 = new Vec(this.x-vecInstance.x, this.y-vecInstance.y);
+     return {vecInstance2};
+   }
+   get length() {
+     return (Math.sqrt(this.x*this.x + this.y*this.y));
+   }
+  
+}
+/*
+console.log(new Vec(1,2));
+console.log(new Vec(1,2).plus(new Vec(3,4)));
+
+*/
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+
+
+console.log(new Vec(3, 4).length);
+// → 5
